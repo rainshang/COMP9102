5cfda756ae9c89b07c1021d0bbb66522fcc96e78
Ethan <rainshanghsu@qq.com>
2018-04-12 14:07:55 +1000

    initial import for Parser

--- vc.java
+++ vc.java
@@ -1,51 +1,114 @@
 /*
- * vc.java
- *
- * F O R     A S S I G N M E N T    2
- *
- *
- * Jingling Xue, CSE, UNSW, Sydney NSW 2052, Australia.
- *
+ * 
  */
 
 package VC;
 
 import VC.Scanner.Scanner;
 import VC.Scanner.SourceFile;
-import VC.Recogniser.Recogniser;
-import VC.Scanner.Token;
+import VC.Parser.Parser;
+import VC.TreeDrawer.Drawer;
+import VC.TreePrinter.Printer;
+import VC.UnParser.UnParser;
+import VC.ASTs.AST;
 
 public class vc {
 
     private static Scanner scanner;
     private static ErrorReporter reporter;
-    private static Recogniser recogniser;
+    private static Parser parser;
+    private static Drawer drawer;
+    private static Printer printer;
+    private static UnParser unparser;
 
-    private static String inputFilename;
+    private static int drawingAST = 0;
+    private static String inputFilename; 
+    private static String VCFilename = null;
+    private static String ASTFilename = null; 
+
+    private static AST theAST;
+
+    private static void cmdLineOptions() {
+      System.out.println("Usage: java VC.vc [-options] filename");
+      System.out.println();
+      System.out.println("where options include:");
+      System.out.println("	-ast 		    display the AST (without SourcePosition)");
+      System.out.println("	-astp 		    display the AST (with SourcePosition)");
+      System.out.println("	-t file             print the AST into <file>"); 
+      System.out.println("	-u file  	    unparse the AST into <file>"); 
+      System.exit(1);
+    }
 
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.print("Please type the file you want to process:");
-            java.util.Scanner consoleScanner = new java.util.Scanner(System.in);
-            inputFilename = "Recogniser/test_data/" + consoleScanner.nextLine() + ".vc";
-        } else {
-            inputFilename = args[0];
+        int i = 0;
+        String arg;
+
+        while (i < args.length && args[i].startsWith("-")) {
+          arg = args[i++];
+          
+          if (arg.equals("-ast"))
+            drawingAST = 1;
+          else if (arg.equals("-astp"))
+            drawingAST = 2;
+          else if (arg.equals("-u")) {
+            if (i < args.length)
+              VCFilename = args[i++];
+            else {
+              System.out.println("[# vc #]: invalid option " + arg);
+              cmdLineOptions();
+            }
+          } else if (arg.equals("-t")) {
+            if (i < args.length)
+              ASTFilename = args[i++];
+            else {
+              System.out.println("[# vc #]: invalid option " + arg);
+              cmdLineOptions();
+            }
+          }
         }
+        if (i == args.length)
+           cmdLineOptions();
+        else
+           inputFilename = args[i];
+
         System.out.println("======= The VC compiler =======");
 
         SourceFile source = new SourceFile(inputFilename);
 
         reporter = new ErrorReporter();
-        scanner = new Scanner(source, reporter);
+        scanner  = new Scanner(source, reporter);
+        parser   = new Parser(scanner, reporter);
 
-        recogniser = new Recogniser(scanner, reporter);
+        if (ASTFilename == null)
+          ASTFilename = inputFilename + "t";
+        printer = new Printer(ASTFilename);
 
-        recogniser.parseProgram();
+        if (VCFilename == null)
+          VCFilename = inputFilename + "u";
+        unparser = new UnParser(VCFilename);
 
-        if (reporter.numErrors == 0)
-            System.out.println("Compilation was successful.");
-        else
-            System.out.println("Compilation was unsuccessful.");
+        try {
+        theAST = parser.parseProgram();
+        }
+        catch (Exception e) { 
+          System.out.println("PANIC:");
+	  e.printStackTrace(); 
+          System.exit(1);
+        }
+
+        if (reporter.numErrors == 0) {
+           System.out.println ("Compilation was successful.");
+ 	   drawer   = new Drawer();
+           if (drawingAST == 2)
+             drawer.enableDebugging(); // show SourcePosition
+           if (drawingAST != 0)
+             drawer.draw(theAST); // draw the AST
+
+           printer.print(theAST);
+
+           unparser.unparse(theAST);
+        } else
+           System.out.println ("Compilation was unsuccessful.");
     }
 }
 
